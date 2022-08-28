fd2a64ef7557b1f56689a63a8a4961dba212c264
Mariana Arnez <mariana.arnez99@gmail.com>
2022-08-28 16:57:45 -0400

    Guardar los datos en el local storage

--- basic-react/src/App/index.js
+++ basic-react/src/App/index.js
@@ -2,15 +2,21 @@
 import React from "react";
 import { AppUI } from './AppUI' 
 
-const defaultTodos=[
-  {text:'Cortar cebolla', completed:false },
-  {text:'Tormar el curso de intro a react', completed:false},
-  {text:'Llorar con la llorona', completed:true},
-  {text:'Lavar la ropa', completed:true}
-];
+function App() {
+  // Traemos nuestros TODOs almacenados
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
 
-function App(props) {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  if (!localStorageTodos) {
+    // Si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos uno con un array vacío
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    // Si existen TODOs en el localStorage los regresamos como nuestros todos
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -27,18 +33,27 @@ function App(props) {
     });
   }
 
+  // Creamos la función en la que actualizaremos nuestro localStorage
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    // Cada que el usuario interactúe con nuestra aplicación se guardarán los TODOs con nuestra nueva función
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1)
-    setTodos(newTodos);
+    // Cada que el usuario interactúe con nuestra aplicación se guardarán los TODOs con nuestra nueva función
+    saveTodos(newTodos);
   };
   return (
     <AppUI
--- basic-react/src/ToDoSearch/index.js
+++ basic-react/src/ToDoSearch/index.js
@@ -3,7 +3,6 @@ import './TodoSearch.css';
 
 function TodoSearch({ searchValue, setSearchValue }) {  
   const onSearchValueChange = (event) => {
-    console.log(event.target.value);
     setSearchValue(event.target.value);
   };
 
